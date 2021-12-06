f6c42ddd562e732d42b06ed9cd9f2d49cc59e729
Patrick Dolan <ptdolan@stanford.edu>
2021-12-06 13:35:52 -0500

    insertional library design first draft

--- Code/python/insPrimeProject/.ipynb_checkpoints/inPrimer-checkpoint.py
+++ Code/python/insPrimeProject/.ipynb_checkpoints/inPrimer-checkpoint.py
@@ -1,6 +1,6 @@
-#delPrime.py: script for designing deletional primer pools
+#inPrime.py: script for designing insertional primer pools
 #author: Patrick T. Dolan, QVEU-NIAID-NIH
-#USAGE: python3 delPrime.py ./sequenceFileAsSingleLine.txt OutputFileName.txt <deletionSize int (e.g. 3)> <startSite (1 indexed)> <endSite (1 indexed)> 
+#USAGE: python3 inPrime.py ./sequenceFileAsSingleLine.txt OutputFileName.txt <insertion nt sequence> <startSite (1 indexed)> <endSite (1 indexed)> 
 
 #Imports
 from Bio.SeqUtils import MeltingTemp
@@ -27,11 +27,11 @@ def Tm(primerSeq):
     
     Returns:  all the positions of the deletions
 '''
-def seqWalk(inputSeq,delLength,start=0,end=0):
+def seqWalk(inputSeq,interval=3,start=0,end=0):
     if end==0:
         end=len(inputSeq)-1
-    positions=range(int(len(inputSeq)/delLength))
-    positionsIter=[i*delLength for i in positions if (i >= start/3 and i <= end/3)]
+    positions=range(int(len(inputSeq)/interval))
+    positionsIter=[i*interval for i in positions if (i >= start/interval and i <= end/interval)]
     return(positionsIter)
 
 ''' Function: windowSizing()
@@ -44,7 +44,7 @@ def seqWalk(inputSeq,delLength,start=0,end=0):
         windowSize: minimum primer length (will add bases until TmDes is achieved.
     Returns:  outputString - string for output csv file. 
 '''
-def windowSizing(position,delLength,inputSeq,tmDes,windowSizeMin):
+def windowSizing(position,interval,inputSeq,insertionSeq,tmDes,windowSizeMin):
     #reversePrimer (left)
     inputSeq= inputSeq.upper()
     TM=0.0
@@ -62,40 +62,24 @@ def windowSizing(position,delLength,inputSeq,tmDes,windowSizeMin):
     TM=0.0
     windowSize=windowSizeMin
     while TM<=tmDes:
-        primerF=inputSeq[(position+delLength-2):(windowSize+position-2)]
+        primerF=inputSeq[(position-2):(windowSize+position-2)]
         TM=Tm(primerF)
         windowSize+=1
     
     TM_f=TM #final Tm of forward annealing region.
     
-    #overlap
-    TM=0.0
-    windowSize=windowSizeMin
-    overlap=5
-    while TM<(tmDes+3):
-        Fflank=primerR_for[(len(primerR_for)-overlap):len(primerR_for)]
-        delPrimerF=(Fflank.lower()+primerF)
-        
-        Rflank=primerF[0:overlap]
-        delPrimerR_for=primerR_for+Rflank.lower()
-        
-        forSeq = Seq(delPrimerR_for)
-        delPrimerR = str(forSeq.reverse_complement())
-        overlap+=1
-        
-        TM=Tm(delPrimerF[0:2*(overlap)])
-        
-    TM_OL=TM #final Tm of overlap.
+    #linker annealing
+    linkerSeq_F=primerF.lower()+insertionSeq.upper()+primerR_for.lower()
+    linkerSeq_R=str(Seq(linkerSeq_F).reverse_complement())
     
-    print("Deletion at pos: "+str(position)+" to "+str(position+delLength-1))
-    print("Forward Primer: "+delPrimerF+"\tLength: "+str(len(primerF))+"\tTm: "+str(TM_f))
-    print("Reverse Primer: "+delPrimerR+"\tLength: "+str(len(primerR))+"\tTm: "+str(TM_r))
-    print("TM_OL: "+str(TM_OL))
-    
-    outputString="del_"+str(position)+"-"+str(position+delLength-1)+"_F,"+delPrimerF+","+str(round(TM_f,2))+"\n"+"del_"+str(position)+"-"+str(position+delLength-1)+"_R, "+delPrimerR+","+str(round(TM_r,2))+"\n"#string for text file (e.g. benchling input)
+    print("Insertion at pos: "+str(position))
+    print("Forward Primer: "+primerF+"\tLength: "+str(len(primerF))+"\tTm: "+str(TM_f))
+    print("Reverse Primer: "+primerR+"\tLength: "+str(len(primerR))+"\tTm: "+str(TM_r))
+
+    print("Forward Linker: "+linkerSeq_F+"\tLength: "+str(len(linkerSeq_F)))
+    print("Reverse Linker: "+linkerSeq_R+"\tLength: "+str(len(linkerSeq_R)))
+    outputString="ins_"+str(position)+"_F,"+primerF+","+str(round(TM_f,2))+"\n"+"ins_"+str(position)+"_R, "+primerR+","+str(round(TM_r,2))+"\n"+"ins_"+str(position)+"_linker_F, "+linkerSeq_F+"\n"+"ins_"+str(position)+"_linker_R, "+linkerSeq_R+"\n"#string for text file (e.g. benchling input)
     return(outputString)
-
-
 
 #------------------------------------------------------------------
 #-------------------------------MAIN-------------------------------
@@ -105,10 +89,16 @@ if __name__ == "__main__":
     #defaults
     tmDes=52
     windowSize=8 #minimum priming region length
-
+    
     seqFile=sys.argv[1]
     OF=sys.argv[2]
-    delLength=int(sys.argv[3])
+    interval=int(sys.argv[3])
+    
+    FLAG="GACTACAAAGACGATGACGACAAG"     #DYKDDDDK
+    FLAGx3="GACTATAAAGACCATGATGGAGATTATAAGGACCATGACATCGACTATAAGGATGACGACGATAAA" #DYKDHDG-DYKDHDI-DYKDDDDK
+    SIINFEKL="TCCATTATTAATTTCGAGAAACTG" #SIINFEKL
+    
+    insertionSeq=FLAGx3
     start=int(sys.argv[4])
     end=int(sys.argv[5])
     
@@ -116,9 +106,8 @@ if __name__ == "__main__":
         inputSequence="".join([line for line in IF.readlines()])
 
     with open(OF,"w") as outfile:
-        posArray=seqWalk(inputSequence,delLength,start=start,end=end)
+        posArray=seqWalk(inputSequence,interval,start=start,end=end)
         outfile.write("name,sequence,Tm\n")#header
         for i in posArray:
-            outputString=windowSizing(i, delLength, inputSequence, tmDes, windowSize)
+            outputString=windowSizing(i, interval, inputSequence, insertionSeq, tmDes, windowSize)
             outfile.write(outputString)
-            
