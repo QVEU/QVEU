ba9a5f5c958d2a6ea9486333d4d63e5c4fd1f4d5
williambakhache <williambakhache@hotmail.com>
2021-12-13 16:00:06 -0500

    Update tm.py

--- Code/python/TMcalc/tm.py
+++ Code/python/TMcalc/tm.py
@@ -2,7 +2,7 @@ from Bio.SeqUtils import MeltingTemp
 import sys
 
 def Tm(primerSeq):
-    TM=MeltingTemp.Tm_NN(primerSeq,Na=50, K=50, Mg=1.5,nn_table=MeltingTemp.DNA_NN4)
+    TM=MeltingTemp.Tm_NN(primerSeq,Na=50, K=50, Mg=2,nn_table=MeltingTemp.DNA_NN4)
     return(TM)
 
 if __name__=="__main__":
